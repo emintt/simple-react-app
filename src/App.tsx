db06588e292816fec95dc13653e8369c81361c00
liem nguyen <elminguyen@gmail.com>
2024-05-29 15:33:27 +0300

    search and filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,66 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Component } from 'react';
+import React from 'react';
+import { User } from './types';
 
 
-class App extends Component {
-    constructor() {
-      super();
-      this.state = {
-        name: 'Liem'
-      }
+
+type MyProps = { myProps: string};
+type MyState = { monsters: User[] };
+
+class App extends React.Component<MyProps, MyState> {
+  constructor() {
+    super();
+
+    // initial state
+    this.state = {
+      monsters: [],
+
     }
+  }
+
+  componentDidMount(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) =>
+      this.setState(
+        () => {
+          return { monsters: users };
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+    )
+  }
   render() {
+    console.log('render');
     return (
       <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <p>Hi {this.state.name}</p>
-        <button onClick={() => {
-          this.setState({name: 'Anni'})
-        }}>Change name</button>
+        <div className='App'>
+          <input className='search-box' type="search" placeholder='search monster'
+            onChange={(event) => {
+              console.log({startingArray: this.state.monsters});
+              const searchString = event.target.value.toLocaleLowerCase();
+              const filteredMonsters = this.state.monsters.filter((user) =>  user.name.includes(searchString));
+              console.log(filteredMonsters);
+              this.setState(() => {
+                return { monsters: filteredMonsters }
+              }, () => {
+                console.log({endingArray: this.state.monsters});
 
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+              });
+           }}/>
+          {this.state.monsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );
+          })}
+        </div>
+
       </>
     )
   }
